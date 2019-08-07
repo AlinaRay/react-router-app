8abeb69c80f2ea1dcb33711fe93044ece8e4ac6d
Alina Ray <a.alina.ray@gmail.com>
2019-08-07 16:20:02 +0300

    Updates

---
+++ precache-manifest.94bee8d4218f6249d136b78fbb349815.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aff9549820cd3ac209b75f52525f3329",
+    "url": "/react-router-app/index.html"
+  },
+  {
+    "revision": "d03fccf95583450824d8",
+    "url": "/react-router-app/static/css/main.8015a105.chunk.css"
+  },
+  {
+    "revision": "5b22e84d240713538dcf",
+    "url": "/react-router-app/static/js/2.474835f3.chunk.js"
+  },
+  {
+    "revision": "d03fccf95583450824d8",
+    "url": "/react-router-app/static/js/main.9da5aa26.chunk.js"
+  },
+  {
+    "revision": "80e81a85950596580de4",
+    "url": "/react-router-app/static/js/runtime~main.1f157a88.js"
+  }
+]);
